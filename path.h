0a3629582e3d75abed3916478a48477fd69a936d
Clemence B <clemence.barillot@gmail.com>
2019-02-10 21:53:03 +0100

    Update path.h

--- path.h
+++ path.h
@@ -7,11 +7,8 @@
 int sameDim(Matrix& D, map<int, map<int, int> > Value_Functions, int &mod, vector<vector<int> > &Ef2);
 
 vector<int> EF2(Matrix& D,map<int,int> value_function,int nb_perl,int nb_player,vector<vector<int>>& Ef2);
-<<<<<<< HEAD
 int value_part(map<int,int> value_function, vector<int> part); //pour des value fonction identiques, renvoie la valeur de la part
 int max_vect(vector<int> vect);
 bool EF1(vector<vector<int>> Ef2, map<int,int> value_function, vector<vector<int>> Ef1);
-=======
->>>>>>> parent of 2dc9af6... Graphe perles
 
 #endif // PATH_H
