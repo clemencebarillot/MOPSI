1619a2be048f10614a29ab04eaa3d75ef14f4499
Clemence B <clemence.barillot@gmail.com>
2019-02-10 19:43:58 +0100

    Update path.h

--- path.h
+++ path.h
@@ -7,7 +7,8 @@
 int sameDim(Matrix& D, map<int,int> value_function, int &mod, vector<vector<int> > &Ef2);
 
 vector<int> EF2(Matrix& D,map<int,int> value_function,int nb_perl,int nb_player,vector<vector<int>>& Ef2);
-
-vector<vector<int>> EF1(vector<vector<int>> Ef2, map<int,int> value_function);
+int value_part(map<int,int> value_function, vector<int> part); //pour des value fonction identiques, renvoie la valeur de la part
+int max_vect(vector<int> vect);
+bool EF1(vector<vector<int>> Ef2, map<int,int> value_function, vector<vector<int>> Ef1);
 
 #endif // PATH_H
