254c256e2be4681fdb17880ffe90c777c20c63d4
Clemence B <clemence.barillot@gmail.com>
2019-02-10 19:44:04 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,13 +9,14 @@
 int main()
 {
     map<int,int> value_function;
-    int nb_player=10, nb_perl=100;
+    int nb_player=3, nb_perl=10;
     string path="TestRandom";
 
     vector<vector<int>> Ef2;
+    vector<vector<int>> Ef1;
 
     initiate_data_perl();
-
+/*
     for(nb_perl=100;nb_perl<900;nb_perl+=10){
         if(nb_perl>=300)
             nb_perl+=10;
@@ -36,7 +37,34 @@ int main()
         write_data(path+"Results"+".txt",Ef2);
 
         write_time(nb_perl,(t2-t1)/CLOCKS_PER_SEC);
-    }
+    }*/
+    Matrix S(1,1);
 
+    create_data(path+".txt",nb_player,nb_perl,100);
+    read_file(path+".txt",value_function,nb_player,nb_perl);
+
+
+    clock_t t1=clock();
+    EF2(S,value_function,nb_perl,nb_player,Ef2);
+    clock_t t2=clock();
+    cout<<"Execution time: "<<(t2-t1)/CLOCKS_PER_SEC<<" seconds."<<endl;
+
+
+    write_data(path+"Results"+".txt",Ef2);
+
+    write_time(nb_perl,(t2-t1)/CLOCKS_PER_SEC);
+
+    for(int i=0;i<Ef2.size();i++){
+        cout<<"The perl for the player "<<i<<" are ";
+        for(int j=0;j<Ef2[i].size();j++)
+            cout<<Ef2[i][j]<<" ";
+        cout<<endl;
+        }
+
+    cout<<"calcul EF1 ";
+
+    bool res = EF1(Ef2,value_function,Ef1);
+
+    cout<<res<<endl;
     return 0;
 }
