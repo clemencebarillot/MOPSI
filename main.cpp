79cb916457423f1336deca6484f8c2084e538ec6
Jean-Baptiste <jb.velot@hotmail.fr>
2019-02-10 22:28:58 +0100

    regle t'il le pb ?

--- main.cpp
+++ main.cpp
@@ -8,32 +8,27 @@
 
 int main()
 {
-
     map<int,int> value_function;
-    map<int,map<int,int>> Value_Functions;
-
-    bool sameValueFunction=false;
-
-    int nb_player=3, nb_perl=5;
+    int nb_player=10, nb_perl=100;
     string path="TestRandom";
 
     vector<vector<int>> Ef2;
 
     initiate_data_perl();
 
-//    for(nb_perl=100;nb_perl<900;nb_perl+=10){
-//        if(nb_perl>=300)
-//            nb_perl+=10;
-//        if(nb_perl>=600)
-//            nb_perl+=30;
+    for(nb_perl=100;nb_perl<900;nb_perl+=10){
+        if(nb_perl>=300)
+            nb_perl+=10;
+        if(nb_perl>=600)
+            nb_perl+=30;
         Matrix S(1,1);
 
-        create_data(path+".txt",nb_player,nb_perl,100,sameValueFunction);
-        read_file(path+".txt",Value_Functions,nb_player,nb_perl,sameValueFunction);
+        create_data(path+".txt",nb_player,nb_perl,100);
+        read_file(path+".txt",value_function,nb_player,nb_perl);
 
 
         clock_t t1=clock();
-        EF2(S,Value_Functions,nb_perl,nb_player,Ef2);
+        EF2(S,value_function,nb_perl,nb_player,Ef2);
         clock_t t2=clock();
         cout<<"Execution time: "<<(t2-t1)/CLOCKS_PER_SEC<<" seconds."<<endl;
 
@@ -41,7 +36,7 @@ int main()
         write_data(path+"Results"+".txt",Ef2);
 
         write_time(nb_perl,(t2-t1)/CLOCKS_PER_SEC);
-//    }
+    }
 
     return 0;
 }
