ddf97e16c9dcccec43a14e8d2d58a942fe7df0c7
Jean-Baptiste <jb.velot@hotmail.fr>
2019-02-10 22:47:33 +0100

    mm remarque

--- main.cpp
+++ main.cpp
@@ -8,20 +8,14 @@
 
 int main()
 {
-
     map<int,int> value_function;
-    map<int,map<int,int>> Value_Functions;
-
-    bool sameValueFunction=false;
-
-    int nb_player=3, nb_perl=5;
+    int nb_player=10, nb_perl=100;
     string path="TestRandom";
 
     vector<vector<int>> Ef2;
-    vector<vector<int>> Ef1;
 
     initiate_data_perl();
-/*
+
     for(nb_perl=100;nb_perl<900;nb_perl+=10){
         if(nb_perl>=300)
             nb_perl+=10;
@@ -29,12 +23,12 @@ int main()
             nb_perl+=30;
         Matrix S(1,1);
 
-        create_data(path+".txt",nb_player,nb_perl,100,sameValueFunction);
-        read_file(path+".txt",Value_Functions,nb_player,nb_perl,sameValueFunction);
+        create_data(path+".txt",nb_player,nb_perl,100);
+        read_file(path+".txt",value_function,nb_player,nb_perl);
 
 
         clock_t t1=clock();
-        EF2(S,Value_Functions,nb_perl,nb_player,Ef2);
+        EF2(S,value_function,nb_perl,nb_player,Ef2);
         clock_t t2=clock();
         cout<<"Execution time: "<<(t2-t1)/CLOCKS_PER_SEC<<" seconds."<<endl;
 
@@ -42,34 +36,7 @@ int main()
         write_data(path+"Results"+".txt",Ef2);
 
         write_time(nb_perl,(t2-t1)/CLOCKS_PER_SEC);
-    }*/
-    Matrix S(1,1);
+    }
 
-    create_data(path+".txt",nb_player,nb_perl,100);
-    read_file(path+".txt",value_function,nb_player,nb_perl);
-
-
-    clock_t t1=clock();
-    EF2(S,value_function,nb_perl,nb_player,Ef2);
-    clock_t t2=clock();
-    cout<<"Execution time: "<<(t2-t1)/CLOCKS_PER_SEC<<" seconds."<<endl;
-
-
-    write_data(path+"Results"+".txt",Ef2);
-
-    write_time(nb_perl,(t2-t1)/CLOCKS_PER_SEC);
-
-//    for(int i=0;i<Ef2.size();i++){
-//        cout<<"The perl for the player "<<i<<" are ";
-//        for(int j=0;j<Ef2[i].size();j++)
-//            cout<<Ef2[i][j]<<" ";
-//        cout<<endl;
-//        }
-
-    cout<<"calcul EF1 ";
-
-    bool res = EF1(Ef2,value_function,Ef1);
-
-    cout<<res<<endl;
     return 0;
 }
